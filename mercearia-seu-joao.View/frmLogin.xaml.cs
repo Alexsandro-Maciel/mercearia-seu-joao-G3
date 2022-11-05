252b97e0bcc2078c6cde37a56202a9e9f14ebd9d
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-04 23:32:31 -0300

    Início da lógica da tela de login.

---
+++ mercearia-seu-joao.View/frmLogin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mercearia_seu_joao.View
+{
+    public partial class frmLogin : Window
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        private void EsqueceuSenha(object sender, MouseButtonEventArgs e)
+        {
+            Mensagens.MensagemContateGerente();
+        }
+
+        private void RealizarLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Fechar(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}
