d1c3f426223af2f748fa6ca0842c531f43e2c375
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-07 23:19:07 -0300

    Criação da biblioteca de consultas do Usuario

---
+++ mercearia-seu-joao.Model/ConsultasUsuario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+public static class ConsultasUsuario
+{
+    public static Usuario ObterUsuarioPeloEmailSenha(string email, string senha)
+    {
+        var conexao = new MySqlConnection(ConexaoBD.Connection.ConnectionString);
+        Usuario usuario = null;
+        string senhaCriptografada = Criptografia.CriptografarMD5(senha);
+
+        try
+        {
+            conexao.Open();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"Select * from usuario Where email = @email and senha = @senha";
+            comando.Parameters.AddWithValue("@senha", senhaCriptografada);
+            comando.Parameters.AddWithValue("@email", email);
+            var leitura = comando.ExecuteReader();
+
+            while (leitura.Read())
+            {
+                usuario = new Usuario();
+                usuario.id = leitura.GetInt32("id");
+                usuario.nome = leitura.GetString("nome");
+                usuario.tipoUsuario = leitura.GetString("tipoUsuario");
+                usuario.email = leitura.GetString("email");
+                usuario.senha = leitura.GetString("senha");
+                usuario.dataInserido = leitura.GetString("dataInserido");
+                usuario.dataAlterado = leitura.GetString("dataAlterado");
+                usuario.dataExcluido = leitura.GetString("dataExcluido");
+                break;
+            }
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        finally
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+
+        return usuario;
+    }
+}
